e59e8261017fd4bb0cd3fb07246831af5858b763
CTZDev <carlosteranzavaleta@hotmail.com>
2021-07-27 12:26:37 -0500

    DarkTheme and LocalStorage Theme Done!

--- app/js/modules/dark_theme.js
+++ app/js/modules/dark_theme.js
@@ -1,5 +1,46 @@
 const d = document;
 
 export default function darkTheme() {
-  console.log("sdfs");
+  let mq = matchMedia("(prefers-color-scheme: dark)");
+
+  function lightTheme() {
+    d.body.classList.remove("dark-theme");
+    d.body.classList.add("light-theme");
+    changeThemeImage("moon", "Icon Dark Mode");
+    localStorage.setItem("theme", "light");
+  }
+
+  function darkTheme() {
+    d.body.classList.add("dark-theme");
+    d.body.classList.remove("light-theme");
+    changeThemeImage("sun", "Icon Light Mode");
+    localStorage.setItem("theme", "dark");
+  }
+
+  function changeThemeImage(imgSrc, alt) {
+    const $imgDarkTheme = d.querySelector(".dark-theme-img [data-icon-theme]");
+    $imgDarkTheme.setAttribute("src", `./images/icon-${imgSrc}.svg`);
+    $imgDarkTheme.setAttribute("alt", alt);
+  }
+
+  function changeTheme(e) {
+    e.matches ? darkTheme() : lightTheme();
+  }
+  mq.addEventListener("change", changeTheme);
+
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(".dark-theme-img")) {
+      if (d.body.classList.contains("dark-theme")) {
+        lightTheme();
+      } else {
+        darkTheme();
+      }
+    }
+  });
+
+  d.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("theme") === undefined) localStorage.setItem("theme", "light");
+    if (localStorage.getItem("theme") === "light") lightTheme();
+    if (localStorage.getItem("theme") === "dark") darkTheme();
+  });
 }
--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,9 @@
 import darkTheme from "./modules/dark_theme.js";
+import todoTask from "./modules/task.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
-  darkTheme();
+  todoTask();
 });
+
+darkTheme();
