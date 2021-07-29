ce80c6790dc5a031f76b1effeb12fea64394199e
CTZDev <carlosteranzavaleta@hotmail.com>
2021-07-28 23:09:58 -0500

    Advance Of Tasks - Show and Hidden

--- app/js/modules/task.js
+++ app/js/modules/task.js
@@ -5,6 +5,7 @@ const listTasks = {};
 
 export default function todoTask() {
   const $form = d.getElementById("formTodo");
+
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     const $txtnewTask = $form.txtnewTask.value;
@@ -14,14 +15,24 @@ export default function todoTask() {
   d.addEventListener("DOMContentLoaded", (e) => {
     drawTask();
   });
+
+  d.addEventListener("click", (e) => {
+    taskActions(e);
+    todoTaskOperations(e);
+  });
 }
 
 //Dibujar mis tareas
 const drawTask = () => {
   const $tasksContainer = d.querySelector(".todo-tasks-container");
   const $dataTaskActions = d.querySelectorAll("[data-tasks-actions]");
+  const $countTasks = d.querySelector(".todo-tasks-tags > p > span");
   const $templateTask = d.getElementById("template-task").content;
   const $fragment = d.createDocumentFragment();
+
+  const $btnAllTasks = d.getElementById("btnAllTasks");
+  const $btnActiveTasks = d.getElementById("btnActiveTasks");
+  const $btnCompletedTasks = d.getElementById("btnCompletedTasks");
 
   //Without Task
   if (Object.entries(listTasks).length === 0) {
@@ -33,17 +44,23 @@ const drawTask = () => {
   //With Task
   $tasksContainer.innerHTML = "";
   $dataTaskActions.forEach((task) => task.classList.remove("is-active"));
-  Object.values(listTasks).forEach(({ task, id }) => {
+  Object.values(listTasks).forEach(({ id, task, state, classActive }) => {
     const $clone = $templateTask.cloneNode(true);
     $clone.querySelector(".todo-task-description > p").textContent = task;
-    $clone.querySelector(".todo-task-description > p").dataset.id = id;
+    $clone.querySelector(".todo-task").dataset.state = state;
+    $clone.querySelector(".todo-task").classList.add(`${classActive}`);
+    $clone.querySelector(`.todo-task`).dataset.id = id;
     $fragment.appendChild($clone);
   });
+
+  if ($btnAllTasks.classList.contains("active")) {
+    $countTasks.textContent = Object.values(listTasks).length;
+  }
 
   $tasksContainer.appendChild($fragment);
 };
 
-//Asignar mis tareas al bojeto
+//Assign task to object listTasks
 const setTask = (form, txtnewTask) => {
   //Validity whitespace in the input new Task
   if (txtnewTask === "") {
@@ -55,6 +72,7 @@ const setTask = (form, txtnewTask) => {
     id: Date.now(),
     task: txtnewTask,
     state: false,
+    classActive: "_",
   };
 
   //Object of the Task (Global)
@@ -66,3 +84,110 @@ const setTask = (form, txtnewTask) => {
   //Reset form
   form.reset();
 };
+
+//Tasks
+const taskActions = (e) => {
+  const $countTasks = d.querySelector(".todo-tasks-tags > p > span");
+
+  const $btnActiveTasks = d.getElementById("btnActiveTasks");
+  const $btnCompletedTasks = d.getElementById("btnCompletedTasks");
+
+  //Task selected , completed and incompleted
+  if (e.target.matches(".todo-task-description") || e.target.matches(".todo-task-description > *")) {
+    const $taskDescription = e.target.matches(".todo-task-description");
+    const $todoTask = $taskDescription ? e.target.parentElement : e.target.parentElement.parentElement;
+    let activeTask = "is-active";
+
+    if ($todoTask.dataset.state == "false") {
+      $todoTask.classList.replace("_", `${activeTask}`);
+      $todoTask.dataset.state = "true";
+      listTasks[$todoTask.dataset.id].classActive = `${activeTask}`;
+      listTasks[$todoTask.dataset.id].state = true;
+    } else {
+      $todoTask.classList.replace(`${activeTask}`, "_");
+      $todoTask.dataset.state = "false";
+      listTasks[$todoTask.dataset.id].classActive = "_";
+      listTasks[$todoTask.dataset.id].state = false;
+    }
+
+    if ($btnActiveTasks.classList.contains("active")) {
+      tasksLength(true, $countTasks);
+      showAndHiddenTasks(true);
+    }
+
+    if ($btnCompletedTasks.classList.contains("active")) {
+      tasksLength(false, $countTasks);
+      showAndHiddenTasks(false);
+    }
+  }
+
+  //Delete Task
+  if (e.target.matches(".todo-task-delete")) {
+    const $task = e.target.parentElement;
+    delete listTasks[$task.dataset.id];
+    drawTask();
+  }
+};
+
+//Operations Tasks
+const todoTaskOperations = (e) => {
+  const $btnAllTasks = d.getElementById("btnAllTasks");
+  const $btnActiveTasks = d.getElementById("btnActiveTasks");
+  const $btnCompletedTasks = d.getElementById("btnCompletedTasks");
+  const $detailsTasks = d.querySelectorAll(".todo-details-btn");
+  const $countTasks = d.querySelector(".todo-tasks-tags > p > span");
+  const $btnClearCompleted = d.getElementById("btnClearCompleted");
+
+  if (e.target.matches(".todo-details-btn")) {
+    $detailsTasks.forEach((detail) => {
+      detail.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  }
+
+  if (e.target === $btnAllTasks) {
+    todoAllTasks($detailsTasks);
+  }
+
+  if (e.target === $btnActiveTasks) {
+    drawTask();
+    tasksLength(false, $countTasks);
+    showAndHiddenTasks(true);
+  }
+
+  if (e.target === $btnCompletedTasks) {
+    drawTask();
+    tasksLength(true, $countTasks);
+    showAndHiddenTasks(false);
+  }
+
+  if (e.target === $btnClearCompleted) {
+    console.log("sdfsdfsd");
+  }
+};
+
+const todoAllTasks = () => {
+  drawTask();
+};
+
+//Active and Completed Tasks (length)
+const tasksLength = (active = true, countTask) => {
+  const completedTask = Object.values(listTasks).filter(({ state }) => state === active).length;
+  countTask.textContent = completedTask;
+};
+
+//Show and Hidden Task (Active - Complete)
+const showAndHiddenTasks = (active = true) => {
+  Object.values(listTasks).filter(({ id, state }) => {
+    if (state === active) {
+      d.querySelectorAll(".todo-task").forEach((task) => {
+        const $idTask = task.dataset.id;
+        if (id == $idTask) {
+          task.style.display = "none";
+        }
+      });
+    }
+  });
+};
+
+const deleteTasksCompleted = () => {};
